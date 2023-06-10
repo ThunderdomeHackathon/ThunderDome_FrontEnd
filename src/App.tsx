d070908163180f28c7c71c2dc163dda60686b2df
AlePouroullis <alexpouroullis123@gmail.com>
2023-06-11 01:37:55 +0200

    set up create-react-app project with firebase

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Dunder dome! </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebaseStuff/index.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBht2R-r4U7mQ0ysOanh6ImSIIr90rCAsc",
+  authDomain: "thunderdome-aa580.firebaseapp.com",
+  projectId: "thunderdome-aa580",
+  storageBucket: "thunderdome-aa580.appspot.com",
+  messagingSenderId: "713203365288",
+  appId: "1:713203365288:web:82e111a24c3ea893d0199d",
+  measurementId: "G-3S045CKGTY"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
