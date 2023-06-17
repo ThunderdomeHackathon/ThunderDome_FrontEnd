448bc8812f194d7aecbf1c68880a217a93a246a1
praise-jaravani <jaravanipraise@gmail.com>
2023-06-17 10:48:55 +0000

    Set up general frontend

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,20 @@ import OrgOverview from "./pages/OrgOverview";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import './App.css';
 
 const App: React.FC = () => {
   const [isAuth, setIsAuth] = useState(false);
 
   return (
     <Router>
-      <Routes>
-
-        <Route path="/" element={<Home/>}/>
-
-        <Route path="/org-login" element={<OrgLogin setIsAuth={setIsAuth}/>} />
-
-        <ProtectedRoute isAuth={isAuth} path="/org-overview" element={<OrgOverview/>} />
-
-      </Routes>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/org-login" element={<OrgLogin setIsAuth={setIsAuth}/>} />
+          <ProtectedRoute isAuth={isAuth} path="/org-overview" element={<OrgOverview/>} />
+        </Routes>
+      <Footer />
     </Router>
 
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/Home.css';
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+            <div className="home" style={{ backgroundImage: `url(${"image.jpg"})` }}>
+                <div className="headerContainer">
+                    <div className='subheaderContainer'>
+                    <h1>Valley Farm Secrets</h1>
+                    <p>Fresh and Natural Fruit and Veg!</p>
+                    </div>
+                    <Link to="/products">
+                    <button> View Our Products</button>
+                    </Link>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Home
