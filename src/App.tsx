2647361b44d5b511c74a9548a5b27b5fae01a1ce
praise-jaravani <jaravanipraise@gmail.com>
2023-06-17 10:19:33 +0000

    Created Home Page, Navbar and Footer Components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import OrgLogin from "./pages/OrgLogin";
 import ProtectedRoute from "./components/ProtectedRoute";
 import OrgOverview from "./pages/OrgOverview";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   const [isAuth, setIsAuth] = useState(false);
@@ -11,6 +14,8 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+
+        <Route path="/" element={<Home/>}/>
 
         <Route path="/org-login" element={<OrgLogin setIsAuth={setIsAuth}/>} />
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import {Link} from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import {useState} from "react";
+import '../styles/Navbar.css';
+
+
+function Navbar(){
+
+    const [openLinks, setOpenLinks] = useState(false)
+
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks)
+    }
+
+    return (
+        <div className="navbar">
+            <div className="leftSide" id={openLinks ? "open" : "close"}>
+                <div className="hiddenLinks">
+                    <Link to="/"> Home </Link>
+                    <Link to="/org-login"> Organisation Login </Link>
+                </div>
+            </div>
+            <div className="rightSide">
+                <Link to="/"> Home </Link>
+                <Link to="/org-login"> Organisation Login </Link>
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
